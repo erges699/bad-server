e1c6749691d655096c2aa2e1ac06f1d706fc0849
erges699 <erges699@gmail.com>
2026-01-29 16:31:19 +0300

    20260129-rw031-u-011

--- backend/src/middlewares/admin-guard.ts
+++ backend/src/middlewares/admin-guard.ts
@@ -4,9 +4,9 @@ import { Request, Response, NextFunction } from 'express';
 import UnauthorizedError  from '../errors/unauthorized-error';
 
 export const isAdmin = (req: Request & { user?: { isAdmin: boolean } }, _res: Response, next: NextFunction) => {
-  if (!req.user || !req.user.isAdmin) {
-    return next(new UnauthorizedError('Доступ только для администраторов'));
+  if (!req.user ) {
+    return next(new UnauthorizedError('Доступ только для авторизованных пользователей'));
   }
   next();
 };
-
+// || !req.user.isAdmin
